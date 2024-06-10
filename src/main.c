3a5bc6ca028fc61f18608c1f5ae1537deb52713c
linus-klk <linus.karlssonlk33@gmail.com>
2024-06-10 13:26:15 +0200

    Get last error as a string

--- src/main.c
+++ src/main.c
@@ -26,9 +26,61 @@ LRESULT msg_handler(HWND win, UINT msg, WPARAM w_param, LPARAM l_param)
     return result;
 }
 
+char* get_last_error()
+{
+    DWORD error = GetLastError();
+    if (!error) return NULL;
+
+    char* message = NULL;
+
+    DWORD size = FormatMessageA(
+        FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM |
+            FORMAT_MESSAGE_IGNORE_INSERTS,
+        0, error, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (char*)&message, 0,
+        NULL);
+
+    return message;
+}
+
+int main(int argc, char** argv)
+{
+    WNDCLASS window_class;
+    HWND win;
+    const int width = 600;
+    const int height = 300;
+
+    window_class.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
+    window_class.lpfnWndProc = msg_handler;
+    window_class.hInstance = GetModuleHandle(0);
+    window_class.lpszClassName = "filetic";
+
+    ATOM res = RegisterClass(&window_class);
+    char* message = get_last_error();
+    assert(res);
+
+    win = CreateWindowEx(0, window_class.lpszClassName, "FileTic",
+                         WS_OVERLAPPEDWINDOW | WS_VISIBLE, 10, 10, width,
+                         height, 0, 0, window_class.hInstance, 0);
+
+    assert(win);
+
+    running = true;
+    while (running)
+    {
+        MSG msg;
+        while (PeekMessage(&msg, win, 0, 0, PM_REMOVE))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+}
+
+#if 0
 int WINAPI WinMain(HINSTANCE instance, HINSTANCE prev_instance, LPSTR cmd_line,
                    int show_cmd)
 {
+    
     WNDCLASS window_class = {0};
     HWND win;
     const int width = 600;
@@ -62,3 +114,4 @@ int WINAPI WinMain(HINSTANCE instance, HINSTANCE prev_instance, LPSTR cmd_line,
     return 0;
 }
 
+#endif
