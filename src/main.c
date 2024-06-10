5b98789bdea6d213e5528d7d6d06c4839f7b1d43
linus-klk <linus.karlssonlk33@gmail.com>
2024-06-10 11:40:52 +0200

    Setup window class

--- src/main.c
+++ src/main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
+
+#include "define.h"
 
 LRESULT msg_handler(HWND win, UINT msg, WPARAM w_param, LPARAM l_param)
 {
@@ -10,6 +11,16 @@ LRESULT msg_handler(HWND win, UINT msg, WPARAM w_param, LPARAM l_param)
 
 int WINAPI WinMain(HINSTANCE instance, HINSTANCE prev_instance, LPSTR cmd_line, int show_cmd)
 {
+    WNDCLASS window_class;
+    HWND win;
+    const u16 width = 600;
+    const u16 height = 300;
+
+    window_class.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
+    window_class.lpfnWndProc = msg_handler;
+    window_class.hInstance = instance;
+    window_class.lpszClassName = "filetic";
+
     return 0;
 }
 
