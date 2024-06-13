ee6a19f5837966fd64aa5a7c5dbbb51e0c49fae5
linus-klk <linus.karlssonlk33@gmail.com>
2024-06-13 08:49:57 +0200

    Integrated thread queue

---
+++ src/thread_queue.c
@@ -0,0 +1,146 @@
+#include "thread_queue.h"
+#include "platform/platform.h"
+#include <stdlib.h>
+
+u32 global_thread_count = 0;
+
+ThreadTask thread_task(void (*task_callback)(void* data), void* data)
+{
+    ThreadTask task = { .task_callback = task_callback, .data = data };
+    return task;
+}
+
+void semaphore_counter_wait(SemaphoreCounter* semaphore_counter)
+{
+    if (semaphore_counter->count)
+    {
+        ftic_assert(semaphore_counter->semaphore);
+        for (u32 i = 0; i < semaphore_counter->count; i++)
+        {
+            platform_semaphore_wait_and_decrement(semaphore_counter->semaphore);
+        }
+        semaphore_counter->count = 0;
+    }
+}
+
+void semaphore_counter_wait_and_free(SemaphoreCounter* semaphore_counter)
+{
+    semaphore_counter_wait(semaphore_counter);
+    platform_semaphore_destroy(semaphore_counter->semaphore);
+    free(semaphore_counter->semaphore);
+}
+
+void thread_task_push_(ThreadTaskQueue* task_queue, ThreadTask task,
+                       FTicSemaphore* semaphore)
+{
+    platform_semaphore_wait_and_decrement(&task_queue->mutex);
+
+    ftic_assert(task_queue->size < task_queue->capacity);
+
+    task_queue->tail %= task_queue->capacity;
+    task_queue->tasks[task_queue->tail] =
+        (ThreadTaskInternal){ .task = task, .semaphore = semaphore };
+
+    task_queue->tail++;
+    task_queue->size++;
+
+    platform_semaphore_increment(&task_queue->mutex);
+
+    platform_semaphore_increment(&task_queue->start_semaphore);
+}
+
+void thread_tasks_push(ThreadTaskQueue* task_queue, ThreadTask* tasks,
+                       u32 task_count, SemaphoreCounter* semaphore_counter)
+{
+    if (semaphore_counter)
+    {
+        if (!semaphore_counter->semaphore && task_count)
+        {
+            semaphore_counter->semaphore =
+                (FTicSemaphore*)calloc(1, sizeof(FTicSemaphore));
+            *semaphore_counter->semaphore =
+                platform_semaphore_create(0, task_count);
+        }
+        semaphore_counter->count = task_count;
+
+        for (u32 i = 0; i < task_count; i++)
+        {
+            thread_task_push_(task_queue, tasks[i],
+                              semaphore_counter->semaphore);
+        }
+    }
+}
+
+internal ThreadTaskInternal thread_task_pop(ThreadTaskQueue* task_queue)
+{
+    platform_semaphore_wait_and_decrement(&task_queue->mutex);
+
+    ftic_assert(task_queue->size > 0);
+
+    task_queue->head %= task_queue->capacity;
+    ThreadTaskInternal task = task_queue->tasks[task_queue->head];
+
+    task_queue->head++;
+    task_queue->size--;
+
+    platform_semaphore_increment(&task_queue->mutex);
+    return task;
+}
+
+thread_return_value thread_loop(void* data)
+{
+    ThreadAttrib* attrib = (ThreadAttrib*)data;
+
+    for (;;)
+    {
+        platform_semaphore_wait_and_decrement(attrib->start_semaphore);
+
+        ThreadTaskInternal task = thread_task_pop(attrib->queue);
+        task.task.task_callback(task.task.data);
+
+        if (task.semaphore)
+        {
+            platform_semaphore_increment(task.semaphore);
+        }
+    }
+}
+
+void thread_init(u32 capacity, u32 thread_count, ThreadQueue* queue)
+{
+    ftic_assert(!queue->pool);
+    if (thread_count > 8)
+    {
+        thread_count = 8;
+    }
+    queue->pool =
+        (FTicThreadHandle*)calloc(thread_count, sizeof(FTicThreadHandle));
+    queue->pool_size = thread_count;
+    queue->attribs = (ThreadAttrib*)calloc(thread_count, sizeof(ThreadAttrib));
+    global_thread_count = thread_count;
+
+    FTicSemaphore start_semaphore = platform_semaphore_create(0, capacity);
+    FTicSemaphore mutex = platform_semaphore_create(1, capacity);
+    queue->task_queue.start_semaphore = start_semaphore;
+    queue->task_queue.mutex = mutex;
+    queue->task_queue.capacity = capacity;
+    queue->task_queue.tasks = (ThreadTaskInternal*)calloc(
+        queue->task_queue.capacity, sizeof(ThreadTaskInternal));
+
+    for (u32 i = 0; i < thread_count; i++)
+    {
+        ThreadAttrib* ta = queue->attribs + i;
+        ta->start_semaphore = &queue->task_queue.start_semaphore;
+        ta->queue = &queue->task_queue;
+        ta->id = i;
+        queue->pool[i] = platform_thread_create(ta, thread_loop, 0, NULL);
+    }
+}
+
+void threads_destroy(ThreadQueue* queue)
+{
+    const u32 thread_count = queue->pool_size;
+    for (u32 i = 0; i < thread_count; i++)
+    {
+        platform_thread_destroy(queue->pool[i]);
+    }
+}
---
+++ src/thread_queue.h
@@ -0,0 +1,56 @@
+#pragma once
+#include "define.h"
+
+typedef struct ThreadTask
+{
+    void (*task_callback)(void* data);
+    void* data;
+} ThreadTask;
+
+typedef struct ThreadTaskInternal
+{
+    ThreadTask task;
+    FTicSemaphore* semaphore;
+} ThreadTaskInternal;
+
+typedef struct SemaphoreCounter
+{
+    FTicSemaphore* semaphore;
+    u32 count;
+} SemaphoreCounter;
+
+typedef struct ThreadTaskQueue
+{
+    FTicSemaphore mutex;
+    FTicSemaphore start_semaphore;
+    ThreadTaskInternal* tasks;
+    u32 capacity;
+    volatile u32 size;
+    volatile u32 head;
+    volatile u32 tail;
+} ThreadTaskQueue;
+
+typedef struct ThreadAttrib
+{
+    FTicSemaphore* start_semaphore;
+    ThreadTaskQueue* queue;
+    u32 id;
+} ThreadAttrib;
+
+typedef struct ThreadQueue
+{
+    u32 pool_size;
+    FTicThreadHandle* pool;
+    ThreadAttrib* attribs;
+    ThreadTaskQueue task_queue;
+} ThreadQueue;
+
+#define THREAD_TASK_ENTRY_POINT(function_name) void function_name(void* data)
+ThreadTask thread_task(void (*task_callback)(void* data), void* data);
+void semaphore_counter_wait(SemaphoreCounter* semaphore_counter);
+void semaphore_counter_wait_and_free(SemaphoreCounter* semaphore_counter);
+void thread_tasks_push(ThreadTaskQueue* task_queue, ThreadTask* tasks, u32 task_count, SemaphoreCounter* semaphore_counter);
+void thread_init(u32 capacity, u32 thread_count, ThreadQueue* queue);
+void threads_destroy(ThreadQueue* queue);
+
+extern u32 global_thread_count;
